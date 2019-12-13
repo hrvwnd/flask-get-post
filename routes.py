09c3862a341dfc61af12c5ee7777a6e92298c4a3
hrvwnd <harveyawendon@gmail.com>
2019-12-13 09:41:36 +0000

    first push

---
+++ routes.py
@@ -0,0 +1,21 @@
+from flask import Flask, request
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def get_Test():
+return {"name": "Bob"}
+
+
+@app.route('/post_test', methods=['POST'])
+def post_test():
+    print (request.json)
+    return "ok \n"
+
+if __name__ == '__main__'
+app.run()
+
+
+
+
